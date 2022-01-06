65b808505e4db85c2caf2c49e8ddfa85cab968ad
ValentinaGrin <vbhygiene@gmail.com>
2022-01-06 10:52:01 -0600

    Created new project, First commit

---
+++ src/Package1/AssignmentOne.java
@@ -0,0 +1,14 @@
+package Package1;
+
+import java.sql.SQLOutput;
+
+public class AssignmentOne {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hallo, hello!!!");
+    }
+
+
+}
